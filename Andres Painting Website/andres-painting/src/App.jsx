b2ceb0d493efdeb0d952f0d02de182c8760ff009
Bryan Rodriguez <bryanrodriguez@Bryans-MacBook-Pro.local>
2023-11-03 21:37:16 -0700

    intial project setup for Andres painting

---
+++ Andres Painting Website/andres-painting/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return(
+      <>
+        <h1>Valdivia Painting</h1>
+        <p>TODO</p>
+        <p>set up routing</p>
+        <p>set up responsive web page</p>
+        <p>how to add images</p>
+        <p>use the mui</p>
+      </>
+    );
+  }
+}
+
+export default App;
