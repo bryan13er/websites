accb1c7f064d2582dc52322b0e7f596196967875
Bryan Rodriguez <bryanrodriguez@Bryans-MacBook-Pro.local>
2023-11-05 21:05:58 -0800

    updated to high res image fixed some spacing

--- Andres Painting Website/andres-painting/src/Component/Pages/Home.jsx
+++ Andres Painting Website/andres-painting/src/Component/Pages/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
         <div className="words">
           <h1> Expert Professional Located in Monterey </h1>
           <p className="mainBlurb">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto vitae recusandae voluptates eveniet, quidem velit ab animi. Accusantium error, ducimus neque illum blanditiis reiciendis corrupti sunt, dicta dolorem asperiores eius laborum. Cupiditate eaque magnam aut dolorum qui suscipit sunt libero alias nulla recusandae laudantium, non sapiente omnis beatae, voluptas ab, labore corporis voluptatum architecto fuga nemo adipisci quas! Libero, quia exercitationem, ab sequi nobis aperiam soluta sint ex sit quibusdam assumenda? Error laudantium quidem omnis quia, amet vitae unde nisi veritatis dicta harum, consequuntur culpa voluptates laboriosam velit obcaecati facilis? Non nulla id, officiis repudiandae hic inventore odio sequi reprehenderit?
+            Lorem ipsum dgolor sit amet, consectetur adipisicing elit. Architecto vitae recusandae voluptates eveniet, quidem velit ab animi. Accusantium error, ducimus neque illum blanditiis reiciendis corrupti sunt, dicta dolorem asperiores eius laborum. Cupiditate eaque magnam aut dolorum qui suscipit sunt libero alias nulla recusandae laudantium, non sapiente omnis beatae, voluptas ab, labore corporis voluptatum architecto fuga nemo adipisci quas! Libero, quia exercitationem, ab sequi nobis aperiam soluta sint ex sit quibusdam assumenda? Error laudantium quidem omnis quia, amet vitae unde nisi veritatis dicta harum, consequuntur culpa voluptates laboriosam velit obcaecati facilis? 
           </p>
         </div>
         <div className="imageContainer">
