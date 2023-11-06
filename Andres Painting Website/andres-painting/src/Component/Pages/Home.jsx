a071c644e62f757cc90e23e8fbd98d05a9c68210
Bryan Rodriguez <bryanrodriguez@Bryans-MacBook-Pro.local>
2023-11-05 16:48:01 -0800

    working so far, when scrolling down menu buttons are still accesible and not coverd by opacity

--- Andres Painting Website/andres-painting/src/Component/Pages/Home.jsx
+++ Andres Painting Website/andres-painting/src/Component/Pages/Home.jsx
@@ -3,10 +3,18 @@ import "./Home.css"
 
 const Home = () => {
   return (
-    <div className="containerStyle">
-      <h1 className="rollerText">Call US</h1>
-      <p> 831 521 2987</p>
-    </div>
+    <>
+      <div className="containerStyle">
+        <h1 className="rollerText">Call US</h1>
+        <p> 831 521 2987</p>
+      </div >
+      <div className="spacer"></div>
+      <h1> Expert Professional Organized </h1>
+      <div className="spacer"></div>
+      <p className="mainBlurb">   
+        At Handcrafted Finishes by Friday, your satisfaction is our main priority. Since our business started, quality has been our focus. By working with the best materials in the industry and employing the most experienced and meticulous painters, we are able to deliver solid and long lasting solutions for everyone.
+      </p>
+    </>
   );
 }
 
