7f3eb377e3653adb7ec24e0e410fd92cde8a89b0
Bryan Rodriguez <bryanrodriguez@Bryans-MacBook-Pro.local>
2023-11-05 18:55:45 -0800

    workkng on trying to fiex text beneath background image

--- Andres Painting Website/andres-painting/src/Component/Pages/Home.jsx
+++ Andres Painting Website/andres-painting/src/Component/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import "./Home.css"
+import "./Home.css";
+import paint_brushes from "../../images/paint_and_brushes.jpg";
 
 const Home = () => {
   return (
@@ -7,16 +8,26 @@ const Home = () => {
       <div className="containerStyle">
         <h1 className="rollerText">Call US</h1>
         <p> 831 521 2987</p>
-      </div >
-      <div className="spacer"></div>
-      <h1> Expert Professional Organized </h1>
-      <div className="spacer"></div>
-      <p className="mainBlurb">   
-        At Handcrafted Finishes by Friday, your satisfaction is our main priority. Since our business started, quality has been our focus. By working with the best materials in the industry and employing the most experienced and meticulous painters, we are able to deliver solid and long lasting solutions for everyone.
-      </p>
+      </div>
+      <div className="flexContainer">
+        <div className="words">
+          <div className="spacer"></div>
+          <h1> Expert Professional Organized </h1>
+          <div className="spacer"></div>
+          <p className="mainBlurb">
+            At Handcrafted Finishes by Friday, your satisfaction is our main priority. Since our business started, quality has been our focus. By working with the best materials in the industry and employing the most experienced and meticulous painters, we are able to deliver solid and long-lasting solutions for everyone.
+          </p>
+        </div>
+        <div className="imageContainer">
+          <img
+            src={paint_brushes}
+            alt="paint"
+            className="flexImage"
+          />
+        </div>
+      </div>
     </>
   );
-}
+};
 
 export default Home;
-
