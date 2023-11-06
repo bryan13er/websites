b7e2cb45e8e67165c8b5506ef07477899ece29c4
Bryan Rodriguez <bryanrodriguez@Bryans-MacBook-Pro.local>
2023-11-05 20:51:37 -0800

    sample homepage ready

--- Andres Painting Website/andres-painting/src/Component/Pages/Home.jsx
+++ Andres Painting Website/andres-painting/src/Component/Pages/Home.jsx
@@ -1,21 +1,26 @@
 import React from "react";
 import "./Home.css";
-import paint_brushes from "../../images/paint_and_brushes.jpg";
+import paint_brushes from "../../images/painters_group.png";
+import paint_logo from "../../images/temp_logo.png"
 
 const Home = () => {
   return (
     <>
       <div className="containerStyle">
-        <h1 className="rollerText">Call US</h1>
-        <p> 831 521 2987</p>
+        <img
+          src={paint_logo}
+          alt="paint"
+          className="logo"
+        />
+        <h1 className="rollerText">Monterey, CA</h1>
+        <p className="number"> (831) 521-2987</p>
       </div>
+      <div className="spacer"/>
       <div className="flexContainer">
         <div className="words">
-          <div className="spacer"></div>
-          <h1> Expert Professional Organized </h1>
-          <div className="spacer"></div>
+          <h1> Expert Professional Located in Monterey </h1>
           <p className="mainBlurb">
-            At Handcrafted Finishes by Friday, your satisfaction is our main priority. Since our business started, quality has been our focus. By working with the best materials in the industry and employing the most experienced and meticulous painters, we are able to deliver solid and long-lasting solutions for everyone.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto vitae recusandae voluptates eveniet, quidem velit ab animi. Accusantium error, ducimus neque illum blanditiis reiciendis corrupti sunt, dicta dolorem asperiores eius laborum. Cupiditate eaque magnam aut dolorum qui suscipit sunt libero alias nulla recusandae laudantium, non sapiente omnis beatae, voluptas ab, labore corporis voluptatum architecto fuga nemo adipisci quas! Libero, quia exercitationem, ab sequi nobis aperiam soluta sint ex sit quibusdam assumenda? Error laudantium quidem omnis quia, amet vitae unde nisi veritatis dicta harum, consequuntur culpa voluptates laboriosam velit obcaecati facilis? Non nulla id, officiis repudiandae hic inventore odio sequi reprehenderit?
           </p>
         </div>
         <div className="imageContainer">
