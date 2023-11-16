7cb67cf7dcd8d9d49a329adc2587a050fe528ea2
Bryan Rodriguez <bryanrodriguez@Bryans-MacBook-Pro.local>
2023-11-15 21:17:12 -0800

    added card to services and some spacing improvements, moving on to collapsing text during mobile

--- Andres Painting Website/andres-painting/src/Component/Pages/Services.jsx
+++ Andres Painting Website/andres-painting/src/Component/Pages/Services.jsx
@@ -1,12 +1,63 @@
 // Services.js
 import React from "react";
-import "./Services.css"; // Import the CSS file
+import test from "../../images/arial_monterey.jpeg"
+import "./Services.css";
 
 const Services = () => {
   return (
-    <div>
-      Services
-    </div>   
+    <>
+      <div className="titleBackground">
+        <div className="ServicesTitle">Services</div>
+      </div>
+      <div className="services-row">
+        <div className="services-container">
+          <img
+            src={test}
+            alt="monterey"
+            className="topPic"
+          />
+          <div className="text-container">
+            <h1>Title</h1>
+            <p className="service-desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui autem labore voluptates temporibus quidem ducimus, similique dignissimos asperiores cumque nostrum, dicta voluptatem! Voluptatum, quibusdam explicabo.</p>
+          </div>
+        </div>
+        <div className="services-container">
+          <img
+            src={test}
+            alt="monterey"
+            className="topPic"
+          />
+          <div className="text-container">
+            <h1>Title</h1>
+            <p className="service-desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui autem labore voluptates temporibus quidem ducimus, similique dignissimos asperiores cumque nostrum, dicta voluptatem! Voluptatum, quibusdam explicabo.</p>
+          </div>
+        </div>
+      </div>
+      <div className="services-row">
+        <div className="services-container">
+          <img
+            src={test}
+            alt="monterey"
+            className="topPic"
+          />
+          <div className="text-container">
+            <h1>Title</h1>
+            <p className="service-desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui autem labore voluptates temporibus quidem ducimus, similique dignissimos asperiores cumque nostrum, dicta voluptatem! Voluptatum, quibusdam explicabo.</p>
+          </div>
+        </div>
+        <div className="services-container">
+          <img
+            src={test}
+            alt="monterey"
+            className="topPic"
+          />
+          <div className="text-container">
+            <h1>Title</h1>
+            <p className="service-desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui autem labore voluptates temporibus quidem ducimus, similique dignissimos asperiores cumque nostrum, dicta voluptatem! Voluptatum, quibusdam explicabo.</p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
