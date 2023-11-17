d5b8c58244f4394f386360b5a868dce477678244
Bryan Rodriguez <bryanrodriguez@Bryans-MacBook-Pro.local>
2023-11-17 00:08:20 -0800

    finished about page, consider doing a simple form page with contact once agian

--- Andres Painting Website/andres-painting/src/Component/Pages/About.jsx
+++ Andres Painting Website/andres-painting/src/Component/Pages/About.jsx
@@ -1,41 +1,75 @@
 import React from "react";
+import monterey from "../../images/arial_monterey.jpeg"
+import FormatPaintIcon from '@mui/icons-material/FormatPaint';
+import HouseIcon from '@mui/icons-material/House';
+import BrushIcon from '@mui/icons-material/Brush';
+
+import "./About.css"
+
+const values_row = {
+  1: {
+    icon: FormatPaintIcon,
+    title: 'Customer Satifaction',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem sit non voluptas mollitia quidem a dolores autem exercitationem esse placeat?'
+  },
+  2 : {
+    icon: HouseIcon,
+    title: 'Quality Work',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem sit non voluptas mollitia quidem a dolores autem exercitationem esse placeat?'
+  },
+  3: {
+    icon: BrushIcon,
+    title: 'On Time Finish',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem sit non voluptas mollitia quidem a dolores autem exercitationem esse placeat?'
+  },
+};
 
 const About = () => {
+
+  const containers = [];
+
+  for (let key in values_row) {
+    const item = values_row[key];
+    containers.push(
+      <div key={key} className="container values">
+        <div className="large-icon-container values-logo">
+          <item.icon style={{ fontSize: '70px' }}/>
+        </div>
+        <div className="ontainer__text">
+          <h2>{item.title}</h2>
+          <p><br/>{item.text}</p>
+        </div>
+      </div>
+    );
+  }
+
+
   return (
-    <div>
-      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, sed illo eaque necessitatibus provident quod facilis nulla doloribus placeat quae sequi officiis voluptatem delectus ea aperiam voluptatum omnis recusandae quidem deserunt suscipit repellendus et distinctio? Asperiores optio iusto magnam, modi delectus velit nobis omnis quam provident ex, at minima praesentium eum odio quis veniam! Magnam hic delectus illo nulla in dolore necessitatibus reprehenderit maxime, inventore cupiditate quis quo aut iusto impedit minus libero tempore autem numquam sed maiores quia, odio nam qui eligendi? Dolor facere dignissimos commodi veniam dolores quae, quasi impedit. Ratione quis eius molestiae voluptatem reprehenderit cupiditate officiis!</div>
-      <br/>
-      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, sed illo eaque necessitatibus provident quod facilis nulla doloribus placeat quae sequi officiis voluptatem delectus ea aperiam voluptatum omnis recusandae quidem deserunt suscipit repellendus et distinctio? Asperiores optio iusto magnam, modi delectus velit nobis omnis quam provident ex, at minima praesentium eum odio quis veniam! Magnam hic delectus illo nulla in dolore necessitatibus reprehenderit maxime, inventore cupiditate quis quo aut iusto impedit minus libero tempore autem numquam sed maiores quia, odio nam qui eligendi? Dolor facere dignissimos commodi veniam dolores quae, quasi impedit. Ratione quis eius molestiae voluptatem reprehenderit cupiditate officiis!</div>
-      <br/>
-      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, sed illo eaque necessitatibus provident quod facilis nulla doloribus placeat quae sequi officiis voluptatem delectus ea aperiam voluptatum omnis recusandae quidem deserunt suscipit repellendus et distinctio? Asperiores optio iusto magnam, modi delectus velit nobis omnis quam provident ex, at minima praesentium eum odio quis veniam! Magnam hic delectus illo nulla in dolore necessitatibus reprehenderit maxime, inventore cupiditate quis quo aut iusto impedit minus libero tempore autem numquam sed maiores quia, odio nam qui eligendi? Dolor facere dignissimos commodi veniam dolores quae, quasi impedit. Ratione quis eius molestiae voluptatem reprehenderit cupiditate officiis!</div>
-      <br/>
-      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, sed illo eaque necessitatibus provident quod facilis nulla doloribus placeat quae sequi officiis voluptatem delectus ea aperiam voluptatum omnis recusandae quidem deserunt suscipit repellendus et distinctio? Asperiores optio iusto magnam, modi delectus velit nobis omnis quam provident ex, at minima praesentium eum odio quis veniam! Magnam hic delectus illo nulla in dolore necessitatibus reprehenderit maxime, inventore cupiditate quis quo aut iusto impedit minus libero tempore autem numquam sed maiores quia, odio nam qui eligendi? Dolor facere dignissimos commodi veniam dolores quae, quasi impedit. Ratione quis eius molestiae voluptatem reprehenderit cupiditate officiis!</div>
-      <br/>
-      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, sed illo eaque necessitatibus provident quod facilis nulla doloribus placeat quae sequi officiis voluptatem delectus ea aperiam voluptatum omnis recusandae quidem deserunt suscipit repellendus et distinctio? Asperiores optio iusto magnam, modi delectus velit nobis omnis quam provident ex, at minima praesentium eum odio quis veniam! Magnam hic delectus illo nulla in dolore necessitatibus reprehenderit maxime, inventore cupiditate quis quo aut iusto impedit minus libero tempore autem numquam sed maiores quia, odio nam qui eligendi? Dolor facere dignissimos commodi veniam dolores quae, quasi impedit. Ratione quis eius molestiae voluptatem reprehenderit cupiditate officiis!</div>
-      <br/>
-      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, sed illo eaque necessitatibus provident quod facilis nulla doloribus placeat quae sequi officiis voluptatem delectus ea aperiam voluptatum omnis recusandae quidem deserunt suscipit repellendus et distinctio? Asperiores optio iusto magnam, modi delectus velit nobis omnis quam provident ex, at minima praesentium eum odio quis veniam! Magnam hic delectus illo nulla in dolore necessitatibus reprehenderit maxime, inventore cupiditate quis quo aut iusto impedit minus libero tempore autem numquam sed maiores quia, odio nam qui eligendi? Dolor facere dignissimos commodi veniam dolores quae, quasi impedit. Ratione quis eius molestiae voluptatem reprehenderit cupiditate officiis!</div>
-      <br/>
-      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, sed illo eaque necessitatibus provident quod facilis nulla doloribus placeat quae sequi officiis voluptatem delectus ea aperiam voluptatum omnis recusandae quidem deserunt suscipit repellendus et distinctio? Asperiores optio iusto magnam, modi delectus velit nobis omnis quam provident ex, at minima praesentium eum odio quis veniam! Magnam hic delectus illo nulla in dolore necessitatibus reprehenderit maxime, inventore cupiditate quis quo aut iusto impedit minus libero tempore autem numquam sed maiores quia, odio nam qui eligendi? Dolor facere dignissimos commodi veniam dolores quae, quasi impedit. Ratione quis eius molestiae voluptatem reprehenderit cupiditate officiis!</div>
-      <br/>
-      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, sed illo eaque necessitatibus provident quod facilis nulla doloribus placeat quae sequi officiis voluptatem delectus ea aperiam voluptatum omnis recusandae quidem deserunt suscipit repellendus et distinctio? Asperiores optio iusto magnam, modi delectus velit nobis omnis quam provident ex, at minima praesentium eum odio quis veniam! Magnam hic delectus illo nulla in dolore necessitatibus reprehenderit maxime, inventore cupiditate quis quo aut iusto impedit minus libero tempore autem numquam sed maiores quia, odio nam qui eligendi? Dolor facere dignissimos commodi veniam dolores quae, quasi impedit. Ratione quis eius molestiae voluptatem reprehenderit cupiditate officiis!</div>
-      <br/>
-      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, sed illo eaque necessitatibus provident quod facilis nulla doloribus placeat quae sequi officiis voluptatem delectus ea aperiam voluptatum omnis recusandae quidem deserunt suscipit repellendus et distinctio? Asperiores optio iusto magnam, modi delectus velit nobis omnis quam provident ex, at minima praesentium eum odio quis veniam! Magnam hic delectus illo nulla in dolore necessitatibus reprehenderit maxime, inventore cupiditate quis quo aut iusto impedit minus libero tempore autem numquam sed maiores quia, odio nam qui eligendi? Dolor facere dignissimos commodi veniam dolores quae, quasi impedit. Ratione quis eius molestiae voluptatem reprehenderit cupiditate officiis!</div>
-      <br/>
-      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, sed illo eaque necessitatibus provident quod facilis nulla doloribus placeat quae sequi officiis voluptatem delectus ea aperiam voluptatum omnis recusandae quidem deserunt suscipit repellendus et distinctio? Asperiores optio iusto magnam, modi delectus velit nobis omnis quam provident ex, at minima praesentium eum odio quis veniam! Magnam hic delectus illo nulla in dolore necessitatibus reprehenderit maxime, inventore cupiditate quis quo aut iusto impedit minus libero tempore autem numquam sed maiores quia, odio nam qui eligendi? Dolor facere dignissimos commodi veniam dolores quae, quasi impedit. Ratione quis eius molestiae voluptatem reprehenderit cupiditate officiis!</div>
-      <br/>
-      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, sed illo eaque necessitatibus provident quod facilis nulla doloribus placeat quae sequi officiis voluptatem delectus ea aperiam voluptatum omnis recusandae quidem deserunt suscipit repellendus et distinctio? Asperiores optio iusto magnam, modi delectus velit nobis omnis quam provident ex, at minima praesentium eum odio quis veniam! Magnam hic delectus illo nulla in dolore necessitatibus reprehenderit maxime, inventore cupiditate quis quo aut iusto impedit minus libero tempore autem numquam sed maiores quia, odio nam qui eligendi? Dolor facere dignissimos commodi veniam dolores quae, quasi impedit. Ratione quis eius molestiae voluptatem reprehenderit cupiditate officiis!</div>
-      <br/>
-      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, sed illo eaque necessitatibus provident quod facilis nulla doloribus placeat quae sequi officiis voluptatem delectus ea aperiam voluptatum omnis recusandae quidem deserunt suscipit repellendus et distinctio? Asperiores optio iusto magnam, modi delectus velit nobis omnis quam provident ex, at minima praesentium eum odio quis veniam! Magnam hic delectus illo nulla in dolore necessitatibus reprehenderit maxime, inventore cupiditate quis quo aut iusto impedit minus libero tempore autem numquam sed maiores quia, odio nam qui eligendi? Dolor facere dignissimos commodi veniam dolores quae, quasi impedit. Ratione quis eius molestiae voluptatem reprehenderit cupiditate officiis!</div>
-      <br/>
-      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, sed illo eaque necessitatibus provident quod facilis nulla doloribus placeat quae sequi officiis voluptatem delectus ea aperiam voluptatum omnis recusandae quidem deserunt suscipit repellendus et distinctio? Asperiores optio iusto magnam, modi delectus velit nobis omnis quam provident ex, at minima praesentium eum odio quis veniam! Magnam hic delectus illo nulla in dolore necessitatibus reprehenderit maxime, inventore cupiditate quis quo aut iusto impedit minus libero tempore autem numquam sed maiores quia, odio nam qui eligendi? Dolor facere dignissimos commodi veniam dolores quae, quasi impedit. Ratione quis eius molestiae voluptatem reprehenderit cupiditate officiis!</div>
-      <br/>
-      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, sed illo eaque necessitatibus provident quod facilis nulla doloribus placeat quae sequi officiis voluptatem delectus ea aperiam voluptatum omnis recusandae quidem deserunt suscipit repellendus et distinctio? Asperiores optio iusto magnam, modi delectus velit nobis omnis quam provident ex, at minima praesentium eum odio quis veniam! Magnam hic delectus illo nulla in dolore necessitatibus reprehenderit maxime, inventore cupiditate quis quo aut iusto impedit minus libero tempore autem numquam sed maiores quia, odio nam qui eligendi? Dolor facere dignissimos commodi veniam dolores quae, quasi impedit. Ratione quis eius molestiae voluptatem reprehenderit cupiditate officiis!</div>
-      <br/>
-      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, sed illo eaque necessitatibus provident quod facilis nulla doloribus placeat quae sequi officiis voluptatem delectus ea aperiam voluptatum omnis recusandae quidem deserunt suscipit repellendus et distinctio? Asperiores optio iusto magnam, modi delectus velit nobis omnis quam provident ex, at minima praesentium eum odio quis veniam! Magnam hic delectus illo nulla in dolore necessitatibus reprehenderit maxime, inventore cupiditate quis quo aut iusto impedit minus libero tempore autem numquam sed maiores quia, odio nam qui eligendi? Dolor facere dignissimos commodi veniam dolores quae, quasi impedit. Ratione quis eius molestiae voluptatem reprehenderit cupiditate officiis!</div>
-      <br/>
-      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, sed illo eaque necessitatibus provident quod facilis nulla doloribus placeat quae sequi officiis voluptatem delectus ea aperiam voluptatum omnis recusandae quidem deserunt suscipit repellendus et distinctio? Asperiores optio iusto magnam, modi delectus velit nobis omnis quam provident ex, at minima praesentium eum odio quis veniam! Magnam hic delectus illo nulla in dolore necessitatibus reprehenderit maxime, inventore cupiditate quis quo aut iusto impedit minus libero tempore autem numquam sed maiores quia, odio nam qui eligendi? Dolor facere dignissimos commodi veniam dolores quae, quasi impedit. Ratione quis eius molestiae voluptatem reprehenderit cupiditate officiis!</div>
-      <br/>
+   <div>
+    <h2 className="AboutTitle">
+      ABOUT ME
+    </h2>
+    <h1 className="AboutQuote">
+        Painting Your Vision With <span className="RainbowText">Skilled Precision</span>
+      </h1>
+    <img src={monterey} alt="monterey" className="AboutMePic"/>
+    <p className="AboutQuote Values">My Mission</p>
+    <div className="container-wrapper space">
+      {containers}
     </div>
+    <h1 className="AboutQuote">
+      My Story
+    </h1>
+    <div className="split-container">
+      <div className="text-section">
+        <h2 className="bioTitle">With over 20 years of experience</h2>
+        <p className="bioDesc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas esse a, quasi quae autem facilis eveniet optio, ipsa quo, magnam minus magni alias neque sunt commodi mollitia. Commodi minus pariatur eius voluptatem odit alias doloribus nostrum repellat, fugiat doloremque? Voluptas esse ratione corrupti aspernatur sequi! Itaque libero dicta voluptate aliquid?</p>
+      </div>
+      <div className="image-section">
+        <img src={monterey} alt="painter" className="bioPic"/>
+      </div>
+    </div>
+   </div>
   );
 }
 
