946492e14374b9e262298153e484402c7725d998
Bryan Rodriguez <bryanrodriguez@Bryans-MacBook-Pro.local>
2023-11-18 19:30:20 -0800

    redid projects turned into container

--- Andres Painting Website/andres-painting/src/Component/Pages/Projects.jsx
+++ Andres Painting Website/andres-painting/src/Component/Pages/Projects.jsx
@@ -7,42 +7,121 @@ import "./Projects.css";
 import { Carousel } from "../Useful_Comps/Carousel";
 
 const Projects = () => {
-  const slides = [
-    { src: group, alt: "group" },
-    { src: roller, alt: "roller" },
-    { src: s_roller, alt: "roller on wall" },
-  ];
+ 
+  const pData = {
+    pData1: [
+      {
+        title: "Title 1",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit...",
+        sliderPics: [
+          { src: group, alt: "group" },
+          { src: roller, alt: "roller" },
+          { src: s_roller, alt: "roller on wall" }
+        ]
+      },
+      {
+        title: "Title 2",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit...",
+        sliderPics: [
+          { src: group, alt: "group" },
+          { src: roller, alt: "roller" },
+          { src: s_roller, alt: "roller on wall" }
+        ]
+      }
+    ],
+    pData2: [
+      {
+        title: "Title 3",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit...",
+        sliderPics: [
+          { src: group, alt: "group" },
+          { src: roller, alt: "roller" },
+          { src: s_roller, alt: "roller on wall" }
+        ]
+      },
+      {
+        title: "Title 4",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit...",
+        sliderPics: [
+          { src: group, alt: "group" },
+          { src: roller, alt: "roller" },
+          { src: s_roller, alt: "roller on wall" }
+        ]
+      }
+    ],
+    pData3: [
+      {
+        title: "Title 5",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit...",
+        sliderPics: [
+          { src: group, alt: "group" },
+          { src: roller, alt: "roller" },
+          { src: s_roller, alt: "roller on wall" }
+        ]
+      },
+      {
+        title: "Title 6",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit...",
+        sliderPics: [
+          { src: group, alt: "group" },
+          { src: roller, alt: "roller" },
+          { src: s_roller, alt: "roller on wall" }
+        ]
+      }
+    ],
+    pData4: [
+      {
+        title: "Title 7",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit...",
+        sliderPics: [
+          { src: group, alt: "group" },
+          { src: roller, alt: "roller" },
+          { src: s_roller, alt: "roller on wall" }
+        ]
+      },
+      {
+        title: "Title 8",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit...",
+        sliderPics: [
+          { src: group, alt: "group" },
+          { src: roller, alt: "roller" },
+          { src: s_roller, alt: "roller on wall" }
+        ]
+      }
+    ]
+  };
+
+  const renderProjectsItems = (projectsData) => {
+    return projectsData.map((project) => (
+      <div className="ProjectsContainer" key={project.title}>
+        <Carousel data={project.sliderPics} className="Project-Carousel" />
+        <div className="ProjectText">
+          <h1 className="ProjectTitle">{project.title}</h1>
+          <p className="ProjectDesc">{project.desc}</p>
+        </div>
+      </div>
+    ));
+  };
 
   return (
     <div>
       <h1 className="ProjectsTitle">Projects</h1>
       <div className="CenteredText">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget urna bibendum, imperdiet tellus at, lacinia metus. Nulla facilisi.</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget
+          urna bibendum, imperdiet tellus at, lacinia metus. Nulla facilisi.
+        </p>
       </div>
-      <div className="ProjectsContainer">
-        <div className="ProjectImage">
-          <Carousel data={slides} />
+      {Object.keys(pData).map((dataKey) => (
+        <div className="services-row" key={dataKey}>
+          {renderProjectsItems(pData[dataKey])}
         </div>
-        <div className="ProjectText">
-          <h1 className="ProjectTitle">Project Title</h1>
-          <p className="ProjectDesc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta quaerat debitis et nemo reiciendis voluptas excepturi aspernatur! Fugiat vitae fuga fugit vel est cumque magnam!</p>
-        </div>
-      </div>
-      <div className="ProjectsContainer">
-        <div className="ProjectImage">
-          <Carousel data={slides} />
-        </div>
-        <div className="ProjectText">
-          <h1 className="ProjectTitle">Project Title</h1>
-          <p className="ProjectDesc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente doloribus voluptate temporibus provident amet excepturi, error pariatur animi ipsum voluptates qui molestiae quam, itaque dolore.</p>
-        </div>
-      </div>
+      ))}
       <div className="CenteredText">
-        <p>For more recent projects please visit our socials</p>
+        <p>For more recent projects, please visit our socials</p>
       </div>
     </div>
-    
   );
-}
+};
 
 export default Projects;
